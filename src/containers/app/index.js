568d9545a5b0250310d53b63de68318a50d2da5f
Ali Pour Zahmatkesh <mr.bardia@gmail.com>
2018-05-17 16:51:31 +0430

    connect the structure to server-api by socket and test it

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -3,6 +3,49 @@ import { Route, Link } from 'react-router-dom';
 import Home from '../home';
 import About from '../about';
 
+window.IO.socket.on('connect', function() {
+  console.log('connected ;)', window.IO.socket._raw.id);
+});
+
+window.IO.socket.on('message', function(data) {
+  console.log('receive in client room', data);
+});
+
+window.IO.socket.on('receive', function(data) {
+  console.log('receive in client', data);
+});
+
+window.IO.socket.on('receive_error', function(data) {
+  console.log('receive error in client', data);
+});
+
+window.IO.socket.on('disconnect', function(data) {
+  console.log('disconnected', data);
+});
+
+window.IO.socket.on('reconnect_attempt', () => {
+  console.log('reconnectiong ...');
+});
+
+window.IO.socket.request(
+  {
+    method: 'get',
+    url: '/api/partner/send',
+    data: {
+      client: 'yes apz!'
+    },
+    headers: {}
+  },
+  function(response, jwres) {
+    if (jwres.error) {
+      console.log(jwres); // => e.g. 403
+      console.log('can not logout');
+      return;
+    }
+    console.log('response', response);
+  }
+);
+
 const App = () => (
   <div>
     <header>
